3c8b55577b40ccda8b472bc3a93d4f704bb699a5
Henrique Chiacchio <henriquechiacchio7@gmail.com>
2026-08-26 15:10:32 -0300

    Primeiro commit, versão basica do site

---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string;
+  name: string;
+  category: string;
+  description: string;
+  price: number;
+  image: string;
+  ageStatement: string;
+  origin: string;
+  tastingNotes: string[];
+}
